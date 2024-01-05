a6ffc34d61ac9833a785f4d79044c1aa8ef7e5fc
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-05 16:18:38 +0530

    feat: add create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,9 +1,11 @@
 "use server"
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 import { z } from "zod";
 
-export const register = (values: z.infer<typeof RegisterSchema>) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
@@ -12,5 +14,24 @@ export const register = (values: z.infer<typeof RegisterSchema>) => {
     }
   }
 
-  return { success: "Email sent!" }
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use" }
+  }
+
+  await db.user.create({
+    data: {
+      name, email, password: hashedPassword
+    }
+  });
+
+  return { success: "New user created" }
 }
