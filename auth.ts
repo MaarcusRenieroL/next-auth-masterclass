0fe151513b91f1802b0f06a57aea9590f016b7e2
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-01-08 08:13:32 +0530

    feat: add generate token functionality and integrate resend api

--- auth.ts
+++ auth.ts
@@ -13,6 +13,19 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") {
+        return true;
+      }
+
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser?.emailVerified) {
+        return false;
+      }
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
